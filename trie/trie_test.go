b2a63151c3b6e005b2a90b40a5d7f21f0b5bd22f
0xHansLee <hans@lightscale.io>
2023-05-02 14:49:25 +0900

    trie: reduce unit test time (#26918)

--- trie/trie_test.go
+++ trie/trie_test.go
@@ -18,7 +18,6 @@ package trie
 
 import (
 	"bytes"
-	crand "crypto/rand"
 	"encoding/binary"
 	"errors"
 	"fmt"
@@ -1151,8 +1150,8 @@ func TestDecodeNode(t *testing.T) {
 		elems = make([]byte, 20)
 	)
 	for i := 0; i < 5000000; i++ {
-		crand.Read(hash)
-		crand.Read(elems)
+		prng.Read(hash)
+		prng.Read(elems)
 		decodeNode(hash, elems)
 	}
 }
