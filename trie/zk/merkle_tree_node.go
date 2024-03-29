13697dc8305a20c296ec2ae7a5b2060e6d6cd42e
Logan <logan@lightscale.io>
2024-03-29 16:30:21 +0900

    fix: key preimage not saved, invalid format

--- trie/zk/merkle_tree_node.go
+++ trie/zk/merkle_tree_node.go
@@ -160,7 +160,7 @@ func (n *LeafNode) Hash() *zkt.Hash { return n.hash }
 
 // CanonicalValue [type, Key[0], ..., Key[31], mark[0], ..., mark[3], Data[0], ..., Data[32*len(ValuePreimage)], key preimage len]
 // Leaf node does not provide key preimage, so set the length to 0.
-// If you can provide the key preimage, change the last 0 and add the key preimage afterwards. See ZkMerkleStateTrie.Prove
+// If you can provide the key preimage, change the last 0 and add the key preimage afterwards. See CanonicalValueWithKeyPreimage
 func (n *LeafNode) CanonicalValue() []byte {
 	key := make([]byte, 32)
 	copy(key[:], zkt.ReverseByteOrder(n.Key))
@@ -171,6 +171,18 @@ func (n *LeafNode) CanonicalValue() []byte {
 	buf.Write(n.Data())
 	buf.WriteByte(0) // key preimage len
 	return buf.Bytes()
+}
+
+func (n *LeafNode) CanonicalValueWithKeyPreimage(keyPreimage []byte) []byte {
+	value := n.CanonicalValue()
+	if len(keyPreimage) != 0 {
+		byte32 := &zkt.Byte32{}
+		copy(byte32[:], keyPreimage)
+
+		value[len(value)-1] = byte(len(byte32))
+		value = append(value, byte32[:]...)
+	}
+	return value
 }
 
 func (n *LeafNode) Data() []byte {
