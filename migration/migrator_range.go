d6420143dedca54caa6ddd80d6c6f301d324f6af
Pangsu <maruroid@gmail.com>
2024-08-23 14:02:18 +0900

    feat: implement parallel migration

---
+++ migration/migrator_range.go
@@ -0,0 +1,122 @@
+package migration
+
+import (
+	"bytes"
+	"context"
+	"math/big"
+	"runtime"
+
+	"github.com/holiman/uint256"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/trie"
+)
+
+var (
+	NumProcessAccount = runtime.NumCPU()
+	NumProcessStorage = runtime.NumCPU()
+)
+
+var hashSpace = new(big.Int).Exp(common.Big2, common.Big256, nil)
+
+// hashRange is a utility to handle ranges of hashes, Split up the
+// hash-space into sections, and 'walk' over the sections
+type hashRange struct {
+	current *uint256.Int
+	step    *uint256.Int
+}
+
+// newHashRange creates a new hashRange, initiated at the start position,
+// and with the step set to fill the desired 'num' chunks
+func newHashRange(start common.Hash, num uint64) *hashRange {
+	left := new(big.Int).Sub(hashSpace, start.Big())
+	step := new(big.Int).Div(
+		new(big.Int).Add(left, new(big.Int).SetUint64(num-1)),
+		new(big.Int).SetUint64(num),
+	)
+	step256 := new(uint256.Int)
+	step256.SetFromBig(step)
+
+	return &hashRange{
+		current: new(uint256.Int).SetBytes32(start[:]),
+		step:    step256,
+	}
+}
+
+// Next pushes the hash range to the next interval.
+func (r *hashRange) Next() bool {
+	if r.step.IsZero() {
+		return false
+	}
+	next, overflow := new(uint256.Int).AddOverflow(r.current, r.step)
+	if overflow {
+		return false
+	}
+	r.current = next
+	return true
+}
+
+// Start returns the first hash in the current interval.
+func (r *hashRange) Start() common.Hash {
+	return r.current.Bytes32()
+}
+
+// End returns the last hash in the current interval.
+func (r *hashRange) End() common.Hash {
+	// If the end overflows (non divisible range), return a shorter interval
+	next, overflow := new(uint256.Int).AddOverflow(r.current, r.step)
+	if overflow {
+		return common.MaxHash
+	}
+	return next.SubUint64(next, 1).Bytes32()
+}
+
+// hashRangeIterator
+func hashRangeIterator(tr state.Trie, num int, onLeaf func(key, value []byte) error) error {
+	r := newHashRange(common.Hash{}, uint64(num))
+
+	eg, _ := errgroup.WithContext(context.Background())
+	for {
+		startKey := r.Start().Bytes()
+		endKey := r.End().Bytes()
+		eg.Go(func() error {
+			nodeIt, err := tr.NodeIterator(startKey)
+			if err != nil {
+				log.Error("Failed to open node iterator", "root", tr.Hash(), "err", err)
+				return err
+			}
+			iter := trie.NewIterator(nodeIt)
+			for iter.Next() {
+				if bytes.Compare(iter.Key, startKey) == -1 {
+					continue
+				}
+				if bytes.Compare(iter.Key, endKey) == 1 {
+					break
+				}
+				err = onLeaf(iter.Key, iter.Value)
+				if err != nil {
+					return err
+				}
+				if bytes.Equal(iter.Key, endKey) {
+					break
+				}
+			}
+			if iter.Err != nil {
+				log.Error("Failed to traverse state trie", "root", tr.Hash(), "err", iter.Err)
+				return err
+			}
+			return nil
+		})
+
+		if !r.Next() {
+			break
+		}
+	}
+	if err := eg.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
--- migration/migrator_validate.go
+++ migration/migrator_validate.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"golang.org/x/sync/errgroup"
@@ -16,7 +18,8 @@ import (
 )
 
 func (m *StateMigrator) ValidateMigratedState(mptRoot common.Hash, zkRoot common.Hash) error {
-	var accounts uint
+	var accounts atomic.Uint64
+	var slots atomic.Uint64
 
 	eg, _ := errgroup.WithContext(context.Background())
 	eg.Go(func() error {
@@ -26,22 +29,25 @@ func (m *StateMigrator) ValidateMigratedState(mptRoot common.Hash, zkRoot common
 			return nil
 		}
 
-		acctIt, err := openZkNodeIterator(m.zkdb, zkRoot)
+		zkt, err := trie.NewZkMerkleStateTrie(zkRoot, m.zkdb)
 		if err != nil {
+			log.Error("Failed to create zk state trie", "root", zkRoot, "err", err)
 			return err
 		}
-		accIter := trie.NewIterator(acctIt)
 
-		for accIter.Next() {
-			accounts += 1
-			zkAcc, err := types.NewStateAccount(accIter.Value, true)
+		var mu sync.Mutex
+		err = hashRangeIterator(zkt, NumProcessAccount, func(key, value []byte) error {
+			accounts.Add(1)
+			zkAcc, err := types.NewStateAccount(value, true)
 			if err != nil {
 				log.Error("Invalid account encountered during traversal", "err", err)
 				return err
 			}
 
-			addr := common.BytesToAddress(m.readZkPreimage(accIter.Key))
+			addr := common.BytesToAddress(m.readZkPreimage(key))
+			mu.Lock()
 			mptAcc, err := mpt.GetAccount(addr)
+			mu.Unlock()
 			if err != nil || mptAcc == nil {
 				log.Error("Failed to get account in MPT", "address", addr, "err", err)
 				return err
@@ -64,16 +70,19 @@ func (m *StateMigrator) ValidateMigratedState(mptRoot common.Hash, zkRoot common
 					return err
 				}
 
-				storageIt, err := openZkNodeIterator(m.zkdb, zkAcc.Root)
+				zkt, err := trie.NewZkMerkleStateTrie(zkAcc.Root, m.zkdb)
 				if err != nil {
-					log.Error("Failed to open storage iterator", "root", zkAcc.Root, "err", err)
+					log.Error("Failed to create zk state trie", "root", zkRoot, "err", err)
 					return err
 				}
-				storageIter := trie.NewIterator(storageIt)
-				for storageIter.Next() {
-					slot := m.readZkPreimage(storageIter.Key)
-					zktVal := common.TrimLeftZeroes(common.BytesToHash(storageIter.Value).Bytes())
+				var mu sync.Mutex
+				err = hashRangeIterator(zkt, NumProcessStorage, func(key, value []byte) error {
+					slots.Add(1)
+					slot := m.readZkPreimage(key)
+					zktVal := common.TrimLeftZeroes(common.BytesToHash(value).Bytes())
+					mu.Lock()
 					mptVal, err := mpt.GetStorage(addr, slot)
+					mu.Unlock()
 					if err != nil {
 						log.Error("Failed to get storage value in MPT", "err", err)
 						return err
@@ -81,11 +90,20 @@ func (m *StateMigrator) ValidateMigratedState(mptRoot common.Hash, zkRoot common
 					if !bytes.Equal(mptVal, zktVal) {
 						return fmt.Errorf("account %s storage (slot: %s) mismatch. expected %s, got %s", addr, common.Bytes2Hex(slot), common.Bytes2Hex(zktVal), common.Bytes2Hex(mptVal))
 					}
+					return nil
+				})
+				if err != nil {
+					return err
 				}
 			} else if mptAcc.Root != types.EmptyRootHash {
 				return fmt.Errorf("account %s root should be empty root hash. got %s", addr, mptAcc.Root)
 			}
+			return nil
+		})
+		if err != nil {
+			return err
 		}
+
 		return nil
 	})
 
@@ -97,7 +115,7 @@ func (m *StateMigrator) ValidateMigratedState(mptRoot common.Hash, zkRoot common
 		for {
 			select {
 			case <-ticker.C:
-				log.Info("Migrated state validation in progress", "accounts", accounts)
+				log.Info("Migrated state validation in progress", "accounts", accounts.Load(), "slots", slots.Load())
 			case <-ctx.Done():
 				return
 			}
